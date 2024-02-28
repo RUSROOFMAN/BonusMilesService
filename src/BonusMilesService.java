a6b29c90b12d1f79bf509db2665ef12a48764011
RUSROOFMAN <158050509+RUSROOFMAN@users.noreply.github.com>
2024-02-29 00:16:55 +0700

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
         int miles =  price / 20;
         {
-        return miles;
+            return miles;
         }
     }
 }
