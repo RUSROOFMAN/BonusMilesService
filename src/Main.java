538ce30af34bb85607a4db9825693137b714454f
RUSROOFMAN <158050509+RUSROOFMAN@users.noreply.github.com>
2024-02-29 00:04:20 +0700

    Update Main2.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
+        bonusMilesService service = new bonusMilesService();
         int price = 10_000;
         int miles = service.calculate(10000); // должно получиться 500
         System.out.println(miles);
